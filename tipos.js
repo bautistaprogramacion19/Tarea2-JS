e924fb9326c970469200beb04acbb6b51bfc240e
bautistaprogramacion19 <112521583+bautistaprogramacion19@users.noreply.github.com>
2022-09-08 20:12:41 -0300

    tarea2
    JAVASCRIPT

---
+++ tipos.js
@@ -0,0 +1,26 @@
+// Tipos primitivos
+
+// number
+4;
+0;
+
+// string
+"Hola mundo";
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ===> Todas son falsy
+console.log(null === undefined)  false
+
+if (true) {
+    console.log("cumple")
+    
+}  else {
+    console.log("no cumple")
+}
